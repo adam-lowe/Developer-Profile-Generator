cdedd74787cf76e156d89bedf7034b6b61765ae4
Goldpm <lowedavidadam@gmail.com>
2019-11-09 19:05:29 -0500

    added inquirer template

---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    },
+    {
+      type: "checkbox",
+      message: "What languages do you know?",
+      name: "stack",
+      choices: [
+        "HTML", 
+        "CSS", 
+        "JavaScript", 
+        "MySQL"
+      ]
+    },
+    {
+      type: "list",
+      message: "What is your preferred method of communication?",
+      name: "contact",
+      choices: [
+        "email",
+        "phone",
+        "telekinesis"
+      ]
+    }
+  ]).then(function(data) {
+    console.log(data)
+  });
